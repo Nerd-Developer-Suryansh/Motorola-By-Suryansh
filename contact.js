3b2bec45c9076ca117240e3a200264d188d2aaba
Nerd_Developer_Suryansh <suryanshn02@gmail.com>
2024-12-06 11:22:58 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,10 @@
+var login = document.querySelector("#login")
+var signup = document.querySelector("#signup")
+
+login.addEventListener("click", () => {
+
+    alert("You have Looged-in Successfully ✅")
+})
+signup.addEventListener("click", () => {
+    alert("You Signed-up Successfully ✅")
+})
